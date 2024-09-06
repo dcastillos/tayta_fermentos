b05f500c20c0cfc201d9abbe5ebadb31a74861c9
dcastillos <daniel_aries2@msn.com>
2024-09-05 21:05:21 -0500

    Avance final parte Registro

--- Tayta Fermentos/registro.php
+++ Tayta Fermentos/registro.php
@@ -24,69 +24,74 @@
   	<?php include('header.php'); ?>  
     <!-- END nav -->
     
-   
-
 	<section class="ftco-section bg-light min-h-screen flex items-center justify-center" style="margin-top: 20px;">
-	<div class="container">
-		<div class="row justify-content-center">
-			<div class="col-md-7">
-				<div class="contact-wrap w-100 p-md-5 p-4 text-center mx-auto">
-					
-					<!-- Social login buttons -->
-					<div class="mb-4">
-	<p class="text-center mb-3">Elija una opción para registrarse:</p>
-	<div class="row">
-		<div class="col mb-2">
-			<button type="button" class="btn btn-primary btn-block mx-auto" style="max-width: 300px;">
-				<img src="images/google.png" alt="Google" style="width: 25px; margin-right: 10px;"> Ingresar con Google
-			</button>
-		</div>
-		<div class="col">
-			<button type="button" class="btn btn-primary btn-block mx-auto" style="max-width: 300px;">
-				<img src="images/facebook.png" alt="Facebook" style="width: 25px; margin-right: 10px;"> Ingresar con Facebook
-			</button>
-		</div>
-	</div>
-</div>
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-7">
+					<div class="contact-wrap w-100 p-md-5 p-4 text-center mx-auto">
 
-					<h3 class="mb-4">Crear Cuenta</h3>
-					
-					<form class="text-center mx-auto">
-						<!-- Name input -->
-						<div data-mdb-input-init class="form-outline mb-4 text-left mx-auto" style="max-width: 300px;">
-							<input type="text" id="formName" class="form-control" placeholder="Ej.: Juan Pérez" />
-							<label class="form-label" for="formName">Nombre Completo</label>
+						<div class="mb-4">
+							<p class="text-center mb-3">Elija una opción para registrarse:</p>
+							<div class="row">
+								<div class="col mb-2">
+									<button type="button" class="btn btn-primary btn-block mx-auto" style="max-width: 300px;">
+										<img src="images/google.png" alt="Google" style="width: 25px; margin-right: 10px;"> Ingresar con Google
+									</button>
+								</div>
+								<div class="col">
+									<button type="button" class="btn btn-primary btn-block mx-auto" style="max-width: 300px;">
+										<img src="images/facebook.png" alt="Facebook" style="width: 25px; margin-right: 10px;"> Ingresar con Facebook
+									</button>
+								</div>
+							</div>
 						</div>
 
-						<!-- Email input -->
-						<div data-mdb-input-init class="form-outline mb-4 text-left mx-auto" style="max-width: 300px;">
-							<input type="email" id="form2Example1" class="form-control" placeholder="Ej.: ejemplo@mail.com" />
-							<label class="form-label" for="form2Example1">Correo</label>
-						</div>
+						<h3 class="mb-4">Crear Cuenta</h3>
 
-						<!-- Password input -->
-						<div data-mdb-input-init class="form-outline mb-4 text-left mx-auto" style="max-width: 300px;">
-							<input type="password" id="form2Example2" class="form-control" placeholder="Ingrese su contraseña" />
-							<label class="form-label" for="form2Example2">Contraseña</label>
-						</div>
+						<?php if (isset($_GET['error'])): ?>
+							<div class="alert alert-danger">
+								<?php
+								if ($_GET['error'] === 'correo_existe') {
+									echo "Este correo electrónico ya está registrado.";
+								} elseif ($_GET['error'] === 'error_db') {
+									echo "Error al registrar el usuario. Intente de nuevo.";
+								} elseif ($_GET['error'] === 'campos_vacios') {
+									echo "Por favor, completa todos los campos.";
+								}
+								?>
+							</div>
+						<?php endif; ?>
 
-						<!-- Confirm Password input -->
-						<div data-mdb-input-init class="form-outline mb-4 text-left mx-auto" style="max-width: 300px;">
-							<input type="password" id="formConfirmPassword" class="form-control" placeholder="Confirme su contraseña" />
-							<label class="form-label" for="formConfirmPassword">Confirmar Contraseña</label>
-						</div>
+						<form action="procesar_registro.php" method="POST" class="text-center mx-auto">
+							<div data-mdb-input-init class="form-outline mb-4 text-left mx-auto" style="max-width: 300px;">
+								<input type="text" id="formName" name="nombre" class="form-control" placeholder="Ej.: Juan Pérez" required />
+								<label class="form-label" for="formName">Nombre Completo</label>
+							</div>
 
-						<!-- Submit button -->
-						<button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block mb-4 mx-auto" style="max-width: 300px;">Registrarse</button>
-					</form>
-					
-					<!-- Login message -->
-					<p class="mt-4">¿Ya tienes una cuenta? <a href="login.php" class="text-primary">Inicia Sesión</a></p>
+							<div data-mdb-input-init class="form-outline mb-4 text-left mx-auto" style="max-width: 300px;">
+								<input type="email" id="form2Example1" name="email" class="form-control" placeholder="Ej.: ejemplo@mail.com" required />
+								<label class="form-label" for="form2Example1">Correo</label>
+							</div>
+
+							<div data-mdb-input-init class="form-outline mb-4 text-left mx-auto" style="max-width: 300px;">
+								<input type="password" id="form2Example2" name="password" class="form-control" placeholder="Ingrese su contraseña" required />
+								<label class="form-label" for="form2Example2">Contraseña</label>
+							</div>
+
+							<div data-mdb-input-init class="form-outline mb-4 text-left mx-auto" style="max-width: 300px;">
+								<input type="password" id="formConfirmPassword" name="confirm_password" class="form-control" placeholder="Confirme su contraseña" required />
+								<label class="form-label" for="formConfirmPassword">Confirmar Contraseña</label>
+							</div>
+
+							<button type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block mb-4 mx-auto" style="max-width: 300px;">Registrarse</button>
+						</form>
+
+						<p class="mt-4">¿Ya tienes una cuenta? <a href="login.php" class="text-primary">Inicia Sesión</a></p>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
 
 	
 	<?php include('footer.php'); ?>  
